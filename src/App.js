361f03b19fb73a8e547d949a6faf852a2f6a38e6
candacecodes <candacehchoe@gmail.com>
2021-02-17 20:24:44 -0800

    bar chart

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ class App extends React.Component {
 	};
 
 	render() {
-		const { data } = this.state;
+		const { data, country } = this.state;
 
 		return (
 			<div className="container">
 				<Cards data={data} />{" "}
 				<CountryPicker handleCountryChange={this.handleCountryChange} />{" "}
-				<Chart />{" "}
+				<Chart data={data} country={country} />{" "}
 			</div>
 		);
 	}
