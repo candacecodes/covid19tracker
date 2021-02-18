9ae78d63082b205462f37b1bebd2a9c15bb9371d
candacecodes <candacehchoe@gmail.com>
2021-02-17 20:17:31 -0800

    update cards with country change

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,21 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+	let changeableUrl = url;
+
+	if (country) {
+		changeableUrl = `${url}/countries/${country}`;
+	}
 	try {
 		const {
 			data: { confirmed, recovered, deaths, lastUpdate },
-		} = await axios.get(url);
+		} = await axios.get(changeableUrl);
 
 		return { confirmed, recovered, deaths, lastUpdate };
-	} catch (error) {}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const fetchDailyData = async () => {
@@ -27,11 +34,13 @@ export const fetchDailyData = async () => {
 };
 
 export const fetchCountries = async () => {
-    try {
-        const { data: { countries }} = await axios.get(`${url}/countries`);
+	try {
+		const {
+			data: { countries },
+		} = await axios.get(`${url}/countries`);
 
-        return countries.map((country) => country.name)
-    } catch (error) {
-        console.log(error)
-    }
-}
+		return countries.map((country) => country.name);
+	} catch (error) {
+		console.log(error);
+	}
+};
